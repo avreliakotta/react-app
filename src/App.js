18ec9f52dcff925babeaab14eaf2ffc51151dd0a
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-03-02 20:29:21 +0200

    Added linlk to the repo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      
+      <h1>Weather App</h1>
+      <footer>
+        This project coded by Nataliia Melnyk and is <a href="https://github.com/avreliakotta/react-app" target="_blank" rel="noreferrer" > open sourced on GitHubnpm start
+        </a>
+      </footer>
     </div>
   );
 }
