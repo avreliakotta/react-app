fc2ffed7d80d01595c5e2e1fbe1974cb6f05bb6e
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-03-11 09:33:36 +0200

    Change ome CSS properties

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from "react";
-import WeatherIcon from "./WeatherIcon";
+
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
